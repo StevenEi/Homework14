66e0a79afd51d64dde7436f96dc0092b5305588c
Steven Eiselt <seiselts@gmail.com>
2022-01-18 22:00:09 -0800

    JAWS DB addition

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,32 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// // mysql2 login and database name reference
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//     logging: false
+//   }
+// );
+
+// module.exports = sequelize;
+
+// jawsdb usage, above is for local stuff
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// mysql2 login and database name reference
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    logging: false
-  }
-);
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
